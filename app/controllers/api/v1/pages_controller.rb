17038072add0ad52d18e5208b1f35abdc7078109
keyhit <viacheslavkarpenkoju@gmail.com>
2019-10-27 21:53:21 +0200

    Created project and added working with device

---
+++ app/controllers/api/v1/pages_controller.rb
@@ -0,0 +1,8 @@
+module Api
+  module V1
+    class PagesController < ApplicationController
+      def home
+      end
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  namespace :api do
+    namespace :v1 do
+      get 'pages/home'
+      resources :tasks
+      resources :users
+    end
+  end  
+  root to: 'api/v1/pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
