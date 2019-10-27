17e01df489d5181da37b72abe8c2eb4db04186e9
keyhit <viacheslavkarpenkoju@gmail.com>
2019-10-27 22:36:36 +0200

    added parent task id

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task
   include Mongoid::Document
-  field :name,        type: String
-  field :description, type: String 
-  field :parent_task,  type: BSON::ObjectId, default: nil
+  field :name,            type: String
+  field :description,     type: String 
+  field :parent_task_id,  type: BSON::ObjectId, default: nil
   belongs_to :user
 end
